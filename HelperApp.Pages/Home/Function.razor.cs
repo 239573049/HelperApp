ea691393f2f8335d404fc6069daa72b355040659
贺家乐 <hejiale@xymind.cn>
2022-06-13 17:04:46 +0800

    优化功能界面样式

--- HelperApp.Pages/Home/Function.razor.cs
+++ HelperApp.Pages/Home/Function.razor.cs
@@ -3,42 +3,42 @@
 partial class Function
 {
 
-	Card[] _cards = new Card[]
+    Card[] _cards = new Card[]
 {
-		 new Card
-		{
-			Title="PDF合并",
-			Flex=4,
-			Href="/pdf-merge"
+         new Card
+        {
+            Title="PDF合并",
+            Flex=3,
+            Href="/pdf-merge"
 
-		},
-		 new Card
-		{
-			Title="img转PDF",
-			Flex=4,
-			Href="/img-to-pdf"
-		}
+        },
+         new Card
+        {
+            Title="img转PDF",
+            Flex=3,
+            Href="/img-to-pdf"
+        }
 };
 
-	class Card
-	{
-		/// <summary>
-		/// 标题
-		/// </summary>
-		public string? Title { get; set; }
+    class Card
+    {
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string? Title { get; set; }
 
-		/// <summary>
-		/// 大小
-		/// </summary>
-		public int Flex { get; set; }
+        /// <summary>
+        /// 大小
+        /// </summary>
+        public int Flex { get; set; }
 
-		/// <summary>
-		/// 路由
-		/// </summary>
-		public string? Href { get; set; }
-	}
-	protected override void OnInitialized()
-	{
+        /// <summary>
+        /// 路由
+        /// </summary>
+        public string? Href { get; set; }
+    }
+    protected override void OnInitialized()
+    {
 
-	}
+    }
 }
