621b95fb5d946fb4be24ad5905723939eeb0a252
token <239573049@qq.com>
2022-06-15 14:36:59 +0800

    新增web端界面展示

---
+++ HelperApp.WebAssembly/Program.cs
@@ -0,0 +1,12 @@
+using HelperApp.Pages;
+using HelperApp.WebAssembly;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddPages();
+
+await builder.Build().RunAsync();
